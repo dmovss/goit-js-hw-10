d346bfdca3a061a83907e5b0b756411b736696ab
dmovss <d.vovk2018@gmail.com>
2025-03-02 13:36:55 +0000

    12

---
+++ src/js/1-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startButton = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+const datetimePicker = document.getElementById('datetime-picker');
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+let countdownInterval = null;
+
+const startCountdown = () => {
+  countdownInterval = setInterval(() => {
+    const now = new Date();
+    const timeLeft = userSelectedDate - now;
+
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(0, 0, 0, 0);
+      iziToast.success({ title: 'Success', message: 'Countdown finished!' });
+      startButton.disabled = true;
+      datetimePicker.disabled = false;
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(timeLeft);
+      updateTimerDisplay(days, hours, minutes, seconds);
+    }
+  }, 1000);
+
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+};
+
+startButton.addEventListener('click', startCountdown);
+
+const updateTimerDisplay = (days, hours, minutes, seconds) => {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+};
+
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
